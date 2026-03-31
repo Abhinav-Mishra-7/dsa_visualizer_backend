87da924be4554c3aafded97f44105a0837ca8066
Abhinav Mishra <abhinavmishra12514@gmail.com>
2026-03-31 23:40:30 +0530

    Remove github route

--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -15,8 +15,8 @@ authRouter.get('/check', userMiddleware, checkAuth);
 authRouter.post('/logout', logout); 
 authRouter.post('/phone/send-otp', sendPhoneOtp);
 authRouter.post('/phone/verify-otp', verifyPhoneOtp);
-authRouter.get('/github', githubStart);
-authRouter.get('/github/callback', githubCallback);
+// authRouter.get('/github', githubStart);
+// authRouter.get('/github/callback', githubCallback);
 authRouter.get('/me', getMe);
 
 module.exports = authRouter;
